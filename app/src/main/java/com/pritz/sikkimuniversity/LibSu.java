e4251db4f4e8ddcef58c27dcf6aa19280a35a578
pritz10 <pritzzzzz101010@gmail.com>
2017-03-16 01:31:15 -0700

    part 8

--- app/src/main/java/com/pritz/sikkimuniversity/LibSu.java
+++ app/src/main/java/com/pritz/sikkimuniversity/LibSu.java
@@ -2,18 +2,24 @@ package com.pritz.sikkimuniversity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Display;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import static com.pritz.sikkimuniversity.R.id.webview;
@@ -21,41 +27,66 @@ import static com.pritz.sikkimuniversity.R.id.webview;
 public class LibSu extends AppCompatActivity {
 
     private WebView webView;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lib_su);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-       webView=(WebView)findViewById(webview);
-        //webView.loadUrl("http://slashdot.org/");
+        webView = (WebView) findViewById(R.id.webview);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        webView.setWebViewClient(new myWebClient());
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setBuiltInZoomControls(true);
 
-
-        String summary = "<html><body><center>Hello Man There is no Internet <b>Have a nice day !</b> points.</body></html>";
-      // webView.loadData(summary, "text/html", null);
-       // getWindow().requestFeature(Window.FEATURE_PROGRESS);
-
-        webView.getSettings().setJavaScriptEnabled(true);
-
-        final Activity activity = this;
-        webView.setWebChromeClient(new WebChromeClient() {
-            public void onProgressChanged(WebView view, int progress) {
-
-                activity.setProgress(progress * 1000);
-            }
-        });
-        webView.setWebViewClient(new WebViewClient() {
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
-            }
-        });
-        webView.loadUrl("http://14.139.206.50/w27/ /");
-
-
-
+        webView.loadUrl("http://14.139.206.50/W27/login.aspx/");
+        webView.clearCache(true);
+        webView.clearHistory();
 
     }
 
+    public class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                       super.onPageStarted(view, url, favicon);
+        }
 
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                       view.loadUrl(url);
+            return true;
+
+        }
+        @Override
+        public void onReceivedError(WebView view, int errorCode,
+                                    String description, String failingUrl) {
+                  Toast.makeText(getApplicationContext(), "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            //super.onReceivedError(view, errorCode, description, failingUrl);
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+
+            super.onPageFinished(view, url);
+
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
+
+  /*  @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        else
+        {
+            finish();
+            return true;
+        }
+    }*/
 }
+
--- app/src/main/java/com/pritz/sikkimuniversity/SUSA.java
+++ app/src/main/java/com/pritz/sikkimuniversity/SUSA.java
@@ -1,17 +1,23 @@
 package com.pritz.sikkimuniversity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import static com.pritz.sikkimuniversity.R.id.webview;
 
 public class SUSA extends AppCompatActivity {
     private WebView webView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +25,60 @@ public class SUSA extends AppCompatActivity {
         setContentView(R.layout.activity_lib_su);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        webView = (WebView) findViewById(webview);
+        webView = (WebView) findViewById(R.id.webview);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        webView.setWebViewClient(new SUSA.myWebClient());
+        webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setBuiltInZoomControls(true);
 
-        webView.getSettings().setJavaScriptEnabled(true);
-
-
         webView.loadUrl("http://sikkimuniversitystudentsassociation.in/");
+        webView.clearCache(true);
+        webView.clearHistory();
+
+    }
+
+    public class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+
+        }
+        @Override
+        public void onReceivedError(WebView view, int errorCode,
+                                    String description, String failingUrl) {
+            Toast.makeText(getApplicationContext(), "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            //super.onReceivedError(view, errorCode, description, failingUrl);
+        }
+        @Override
+        public void onPageFinished(WebView view, String url) {
+
+            super.onPageFinished(view, url);
+
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
+
+   @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        else
+        {
+            finish();
+            return true;
+        }
+    }
 
 
     }
-}
