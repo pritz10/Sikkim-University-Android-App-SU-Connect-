fb509d125e857c91fa2d5880b4ed7d40535d1a3f
pritz10 <pritzzzzz101010@gmail.com>
2017-03-30 01:09:54 -0700

    JUST NOW

--- app/src/main/java/com/pritz/sikkimuniversity/MainActivity.java
+++ app/src/main/java/com/pritz/sikkimuniversity/MainActivity.java
@@ -45,6 +45,9 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
     }
 
+
+
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
