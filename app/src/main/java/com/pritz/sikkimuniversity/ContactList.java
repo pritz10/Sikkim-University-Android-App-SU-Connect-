5bd51e0aa78f1bf582101154d359fdaf5f876558
pritz10 <pritzzzzz101010@gmail.com>
2017-06-02 13:21:47 +0530

    DO NOT TOUCH ALRIGHT

--- app/src/main/java/com/pritz/sikkimuniversity/ContactList.java
+++ app/src/main/java/com/pritz/sikkimuniversity/ContactList.java
@@ -9,5 +9,6 @@ public class ContactList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
+        int a;
     }
 }
